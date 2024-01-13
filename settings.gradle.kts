0299d23716e0c3ccbe35608c2f83a45576c4edaa
Akmal Alikhujaev <akmalalikhujaev@gmail.com>
2024-01-13 19:59:49 +0100

    Init server java project

---
+++ settings.gradle.kts
@@ -0,0 +1,2 @@
+rootProject.name = "dnsforwarder.server"
+
---
+++ src/main/java/io/github/akmal2409/Server.java
@@ -0,0 +1,8 @@
+package io.github.akmal2409;
+
+public class Server {
+
+  public static void main(String[] args) {
+    // TODO: Implement bootstrap
+  }
+}
